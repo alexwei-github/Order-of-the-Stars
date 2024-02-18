f58a7b1af9f5ff721537915f9702c886845711aa
Alex Wei <alexwei1818@gmail.com>
2024-02-18 01:47:57 -0800

    Update CombatManager.cs

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CombatManager : MonoBehaviour
 {
@@ -12,6 +13,36 @@ public class CombatManager : MonoBehaviour
 
     public event UnityAction OnHealthChange;
     public static event UnityAction<Character> OnDie;
+
+    public void TakeDamage (int damageToTake){
+        CurHp -= damageToTake;
+
+        OnHealthChange?.Invoke();
+
+        if(CurHp <= 0)
+            Die();
+    }
+
+    void Die(){
+        OnDie?.Invoke(this);
+        //Destroy(gameObject);
+        SceneManager.LoadScene(0);
+    }
+
+    public void Heal (int healAmount){
+        CurHp += healAmount;
+
+        OnHealthChange?.Invoke();
+
+        if(CurHp>MaxHp){
+            CurHp = MaxHp;
+        }
+    }
+
+    public float GetHealthPercentage(){
+        return (float)CurHp/(float)MaxHp;
+    }
+
 
     // Update is called once per frame
     void Update()
