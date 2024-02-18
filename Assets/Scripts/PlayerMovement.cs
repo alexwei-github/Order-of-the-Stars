8d3889b05ba8ba3887395daee82bcf1128e1fa45
nathanyu8th <nathanyu8th@gmail.com>
2024-02-17 19:09:59 -0800

    Basic Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float moveSpeed;
+    public float targetSpeed;
     private Vector2 moveInput;
-    
+
+    public float acceleration;
+    public float decceleration;
+    public float power;
+    private float accelerationRate;
+
+
+    public float jumpForce;
+    private bool isGrounded;
+    private bool isJumping;
+    private float lastGroundedTime;
+    private float lastJumpTime;
+    public float jumpCutForce;
+    public float airRes;
+
 
     void Awake(){
         rb = GetComponent<Rigidbody2D>();
@@ -16,5 +32,62 @@ public class PlayerMovement : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
+    }
+
+    private void FixedUpdate(){
+
+        float targetSpeed = moveInput.x * moveSpeed;
+        float speedDifference = targetSpeed - rb.velocity.x;
+        if((Mathf.Abs(targetSpeed) > 0.01f)){
+            accelerationRate = acceleration;
+        }
+        else{
+            accelerationRate = decceleration;
+        }
+        float movement = Mathf.Pow(Mathf.Abs(speedDifference) * accelerationRate, power) * Mathf.Sign(speedDifference);
+        if(isGrounded){
+            rb.AddForce(movement * Vector2.right);
+        }
+        else{
+            rb.AddForce((movement/airRes) * Vector2.right );
+        }
+
+
+        if(isGrounded && moveInput.y > 0.1f) 
+        {
+            
+            Jump();
+        }
+
+        if(rb.velocity.y > 0 && moveInput.y < 0.1f){
+            jumpCut();
+        }
+        
+    }
+
+    private void Jump(){
+        rb.AddForce(new Vector2(0f, moveInput.y * jumpForce), ForceMode2D.Impulse);
+        //lastGroundedTime = 0;
+        //lastJumpTime = 0;
+        isJumping = true;
+        
+
+    }
+
+    public void jumpCut(){
+        if(rb.velocity.y > 0 && !isGrounded){
+            rb.AddForce(Vector2.down * rb.velocity.y * (1 / jumpCutForce), ForceMode2D.Impulse);    
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.layer == 6){
+            isGrounded = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision){
+        if (collision.gameObject.layer == 6){
+            isGrounded = false;
+        }
     }
 }
