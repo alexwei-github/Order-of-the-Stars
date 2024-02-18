17487b874e0e4d151f14276658c47595c9776887
nathanyu8th <nathanyu8th@gmail.com>
2024-02-18 02:04:49 -0800

    finished checkpoints

--- Assets/Scripts/CheckPointManager.cs
+++ Assets/Scripts/CheckPointManager.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class CheckPointManager : MonoBehaviour
 {
     Vector2 checkPointPos;
+    public CheckPointManager instance;
+
 
     private void Start(){
         checkPointPos = transform.position;
+    }
+
+    public void UpdateCheckpoint(Vector2 pos){
+        checkPointPos = pos;
+        Debug.Log(checkPointPos);
     }
 }
