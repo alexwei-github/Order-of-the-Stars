08947ef633757a3a469818b3985b035ff1bbb1ab
nathanyu8th <nathanyu8th@gmail.com>
2024-02-19 01:38:47 -0800

    boulder pushes

---
+++ Assets/Scripts/Scorpio Traps/Boulder.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Player")){
+            StartCoroutine(Stop());
+            OpenDoor.instance.transform.position = OpenDoor.instance.currentPos;
+            OpenDoor.instance.door.gravityScale = 0;
+        }
+
+    }
+
+    IEnumerator Stop(){
+        PlayerMovement.instance.canMove = false;
+        PlayerMovement.instance.rb.transform.position = CheckPointManager.instance.checkPointPos;
+        PlayerMovement.instance.rb.velocity = new Vector2(0,0);
+        yield return new WaitForSeconds(1f);
+        PlayerMovement.instance.canMove = true;
+    }
+}
