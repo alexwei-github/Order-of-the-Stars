2310f226df82a69615a0c9aa3d480ecad1ff86c0
Alex Wei <alexwei1818@gmail.com>
2024-02-19 17:48:28 -0800

    djoa

---
+++ Assets/Scripts/ConstellationAppear.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstellationAppear : MonoBehaviour
+{
+    public ConstellationChecker checker;
+    public SpriteRenderer sprite;
+    public string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        checker = GameObject.FindGameObjectWithTag("GameController").GetComponent<ConstellationChecker>();
+        switch(name){
+            case "Cancer": 
+                if(checker.Cancer){
+                    sprite.enabled = true;
+                }
+                break;
+            case "Pegasus":
+                if(checker.Pegasus){
+                    sprite.enabled = true;
+                }
+                break;
+            case "Scorpio":
+                if(checker.Scorpio){
+                    sprite.enabled = true;
+                }
+                break;
+            case "Gemini":
+                if(checker.Gemini){
+                    sprite.enabled = true;
+                }
+                break;
+        }
+    }
+}
