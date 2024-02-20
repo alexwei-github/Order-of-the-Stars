3c3ae273e3b5b1f2be231732000b137123aad151
Alex Wei <alexwei1818@gmail.com>
2024-02-20 00:54:41 -0800

    Added Capricorn Scene

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -38,7 +38,7 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.CompareTag("Player")){
-            collision.gameObject.GetComponent<CombatManager>().TakeDamage(5, transform.position, knockbackAmount);
+            collision.gameObject.GetComponent<CombatManager>().TakeDamage(500, transform.position, knockbackAmount);
         }
     }
 }
