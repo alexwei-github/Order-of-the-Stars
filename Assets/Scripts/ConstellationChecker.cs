c06eaf8fb29cf3dafd69e455889b652030712d98
nathanyu8th <nathanyu8th@gmail.com>
2024-02-19 16:26:15 -0800

    Update Scorpio.unity

---
+++ Assets/Scripts/ConstellationChecker.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConstellationChecker : MonoBehaviour
+{
+    public static ConstellationChecker Instance;
+
+    public bool Pegasus, Cancer, Scorpio, Gemini;
+
+
+    private void Awake()
+    {
+        // start of new code
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        // end of new code
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update(){
+        if(SceneManager.GetActiveScene().name.Equals("WorldCreate") && Pegasus && Cancer && Scorpio && Gemini){
+            SceneManager.LoadScene("TheEnd");
+        }
+    }
+
+
+}
